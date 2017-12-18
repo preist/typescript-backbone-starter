9318ee914c51c79b16924d4881099548276809db
Igor <igorputina@gmail.com>
2017-12-18 13:42:48 +0200

    Imported jQuery, Backbone.js and Underscore.js in TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,44 @@
-import { Person } from './person';
+import * as $ from 'jquery';
+import * as _ from 'underscore';
+import * as Backbone from 'backbone';
 
-// Sample
-const johnSmith = new Person("John", "Smith");
-console.log(johnSmith.fullName)
+// Test jQuery
+$(document).ready(function() {
+    console.log("jQuery has loaded correctly!");
+});
+
+// Test Underscore.js
+var mapResult = _.map([1, 2, 3, 4], (number: number) => number * 2);
+
+console.log("Map result is: ", mapResult);
+
+// Test Backbone.js
+class Person extends Backbone.Model {
+
+	defaults() {
+		return {
+			firstName: 'John',
+			lastName: 'Doe'
+		}
+	}
+
+	initialize() {
+		if (!this.get('firstName')) {
+			this.set({ 'firstName': this.defaults().firstName });
+        }
+        
+        if (!this.get('lastName')) {
+			this.set({ 'lastName': this.defaults().lastName });
+		}
+	}
+
+	get fullName() {
+		return this.get('firstName') + " " + this.get('lastName');
+	}
+
+}
+
+var person = new Person({ firstName: "Linus", lastName: "Torvalds" })
+
+console.log(person);
+console.log("Person's full name is: ", person.fullName)
--- src/person.ts
+++
@@ -1,9 +0,0 @@
-export class Person {
-    constructor(public firstName: string, public lastName: string) {
-
-    }
-
-    get fullName(): string {
-        return this.firstName + " " + this.lastName;
-    }
-}
